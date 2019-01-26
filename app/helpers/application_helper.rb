79478a7304110b1ec6ff5f12d00aec7a54266590
Dawid <siema@gmail.com>
2019-01-26 13:02:27 +0100

    helpers done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+
+  def full_title(page_title = '')
+
+    base_title = "iCal Project"
+
+    if page_title.empty?
+
+      base_title
+
+    else
+
+      page_title + " | " + base_title
+
+    end
+
+  end
+
 end
